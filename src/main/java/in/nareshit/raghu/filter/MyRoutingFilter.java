4fa81c9665fb13cf4ffe52dfcd8b93eda56acd90
RAGHU SIR <javabyraghu@gmail.com>
2021-06-22 17:56:08 +0530

    Update MyRoutingFilter.java

--- src/main/java/in/nareshit/raghu/filter/MyRoutingFilter.java
+++ src/main/java/in/nareshit/raghu/filter/MyRoutingFilter.java
@@ -1,11 +1,15 @@
 package in.nareshit.raghu.filter;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
 @Component
@@ -18,7 +22,16 @@ public class MyRoutingFilter extends ZuulFilter {
 	}
 
 	public Object run() throws ZuulException {
-		LOG.info("FROM ROUTING....");
+		RequestContext context = RequestContext.getCurrentContext();
+		HttpServletRequest request = context.getRequest();
+		String auth = request.getHeader("Authorization");
+		if(!StringUtils.hasText(auth)) {
+			LOG.warn("SEND TO LOGIN SERVICE");
+			//RequestDispatcher dispatcher = request.getRequestDispatcher(loginPath);
+		} else {
+			LOG.info("Nice! Authorization Found!!");
+		}
+		
 		return null;
 	}
 
